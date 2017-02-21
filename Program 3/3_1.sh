31134a383446a81f715ebcefc4b3e670e39f55c1
Stanley Cohen <scohe001@delta-19.cs.ucr.edu>
2017-02-21 11:47:01 -0800

    Finished Program 3 part 1

---
+++ Program 3/3_1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -n "Enter first player: "
+read p1
+echo -n "Enter their score: "
+read s1
+
+echo -n "Enter first player: "
+read p2
+echo -n "Enter their score: "
+read s2
+
+echo -n "Enter first player: "
+read p3
+echo -n "Enter their score: "
+read s3
+
+echo "Player 1 - $p1: $s1"
+echo "Player 2 - $p2: $s2"
+echo "Player 3 - $p3: $s3"
+
+
